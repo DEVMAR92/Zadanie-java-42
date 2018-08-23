248d87e10d6d16a70c50791f6e50349aca475c0f
Marcin <Majsyn@gmail.com>
2018-08-23 15:04:22 +0200

    V 0.2

--- src/Test.java
+++ src/Test.java
@@ -6,27 +6,34 @@ public class Test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Queue<Integer> numbers = new LinkedList<>();
-        int[] holder = new int[10];
+
         int sum = 0;
+        int numberSize = 10;
 
         // WCZYTYWANIE LICZB
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < numberSize; i++) {
             System.out.println("Podaj " + (i + 1) + " liczbę");
             numbers.offer(input.nextInt());
-            sum += numbers.peek();
-            holder[i] = numbers.peek();
-            numbers.poll();
+
         }
 
         //WYSWIETLANIE LICZB
-        for (int i = 0; i < holder.length; i++) {
-            if (i == 9) {
-                System.out.println(holder[i] + " = " + sum);
-                break;
-            }
-            System.out.print(holder[i] + " + ");
-        }
-    }
+        for (int i = 0; i < numberSize; i++) {
 
+            sum += numbers.peek();
+            System.out.print(numbers.peek());
+            numbers.poll();
+
+            if (i + 1 == numberSize) {
+                System.out.println(" = " + sum);
+                break;
+            } else {
+                System.out.print(" + ");
+            }
+        }
+
+    }
 }
+
+
 
